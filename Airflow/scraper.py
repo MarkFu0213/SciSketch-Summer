f4305a53d7e19448898d16f7480e2af48b26d415
stevensusas <hanqisu@sas.upenn.edu>
2024-08-18 11:27:42 -0400

    commit

--- Airflow/scraper.py
+++ Airflow/scraper.py
@@ -2,9 +2,12 @@ import requests
 import pandas as pd
 from concurrent.futures import ThreadPoolExecutor, as_completed
 import time
-import json
+import logging
 from requests.adapters import HTTPAdapter
 from requests.packages.urllib3.util.retry import Retry
+from tqdm import tqdm
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 class ScienceDirectAPI:
     def __init__(self, base_url='https://api.elsevier.com/content/search/sciencedirect'):
@@ -20,67 +23,67 @@ class ScienceDirectAPI:
         self.session.mount('https://', HTTPAdapter(max_retries=retries))
         
         self.journals = [
-        "Cell",
-        "Cancer Cell",
-        "Cell Chemical Biology",
-        "Cell Genomics",
-        "Cell Host & Microbe",
-        "Cell Metabolism",
-        "Cell Reports",
-        "Cell Reports Medicine",
-        "Cell Stem Cell",
-        "Cell Systems",
-        "Current Biology",
-        "Developmental Cell",
-        "Immunity",
-        "Med",
-        "Molecular Cell",
-        "Neuron",
-        "Structure",
-        "American Journal of Human Genetics",
-        "Biophysical Journal",
-        "Biophysical Reports",
-        "Human Genetics and Genomics Advances",
-        "Molecular Plant",
-        "Molecular Therapy",
-        "Molecular Therapy Methods & Clinical Development",
-        "Molecular Therapy Nucleic Acids",
-        "Molecular Therapy Oncology",
-        "Plant Communications",
-        "Stem Cell Reports",
-        "Trends in Biochemical Sciences",
-        "Trends in Cancer",
-        "Trends in Cell Biology",
-        "Trends in Ecology & Evolution",
-        "Trends in Endocrinology & Metabolism",
-        "Trends in Genetics",
-        "Trends in Immunology",
-        "Trends in Microbiology",
-        "Trends in Molecular Medicine",
-        "Trends in Neurosciences",
-        "Trends in Parasitology",
-        "Trends in Pharmacological Sciences",
-        "Trends in Plant Science",
-        "Cell Reports Physical Science",
-        "Chem",
-        "Chem Catalysis",
-        "Device",
-        "Joule",
-        "Matter",
-        "Newton",
-        "Trends in Chemistry",
-        "Cell Reports Methods",
-        "Cell Reports Sustainability",
-        "Heliyon",
-        "iScience",
-        "One Earth",
-        "Patterns",
-        "STAR Protocols",
-        "Nexus",
-        "The Innovation",
-        "Trends in Biotechnology",
-        "Trends in Cognitive Sciences"
-    ]
+    "Cell",
+    "Cancer Cell",
+    "Cell Chemical Biology",
+    "Cell Genomics",
+    "Cell Host & Microbe",
+    "Cell Metabolism",
+    "Cell Reports",
+    "Cell Reports Medicine",
+    "Cell Stem Cell",
+    "Cell Systems",
+    "Current Biology",
+    "Developmental Cell",
+    "Immunity",
+    "Med",
+    "Molecular Cell",
+    "Neuron",
+    "Structure",
+    "American Journal of Human Genetics",
+    "Biophysical Journal",
+    "Biophysical Reports",
+    "Human Genetics and Genomics Advances",
+    "Molecular Plant",
+    "Molecular Therapy",
+    "Molecular Therapy Methods & Clinical Development",
+    "Molecular Therapy Nucleic Acids",
+    "Molecular Therapy Oncology",
+    "Plant Communications",
+    "Stem Cell Reports",
+    "Trends in Biochemical Sciences",
+    "Trends in Cancer",
+    "Trends in Cell Biology",
+    "Trends in Ecology & Evolution",
+    "Trends in Endocrinology & Metabolism",
+    "Trends in Genetics",
+    "Trends in Immunology",
+    "Trends in Microbiology",
+    "Trends in Molecular Medicine",
+    "Trends in Neurosciences",
+    "Trends in Parasitology",
+    "Trends in Pharmacological Sciences",
+    "Trends in Plant Science",
+    "Cell Reports Physical Science",
+    "Chem",
+    "Chem Catalysis",
+    "Device",
+    "Joule",
+    "Matter",
+    "Newton",
+    "Trends in Chemistry",
+    "Cell Reports Methods",
+    "Cell Reports Sustainability",
+    "Heliyon",
+    "iScience",
+    "One Earth",
+    "Patterns",
+    "STAR Protocols",
+    "Nexus",
+    "The Innovation",
+    "Trends in Biotechnology",
+    "Trends in Cognitive Sciences"
+]
 
     def get_results(self, query):
         response = self.session.put(self.base_url, headers=self.headers, json=query)
@@ -99,12 +102,13 @@ class ScienceDirectAPI:
                     return self.get_results(query_copy)
                 except requests.exceptions.HTTPError as e:
                     if e.response.status_code == 429:
-                        print(f"Rate limit hit. Waiting for 60 seconds before retrying...")
+                        logging.warning(f"Rate limit hit. Waiting for 60 seconds before retrying...")
                         time.sleep(60)
                     elif e.response.status_code == 400:
-                        print(f"Reached the end of available results at offset {offset}.")
+                        logging.info(f"Reached the end of available results at offset {offset}.")
                         return None
                     else:
+                        logging.error(f"HTTP error occurred: {e}")
                         raise
 
         with ThreadPoolExecutor(max_workers=max_workers) as executor:
@@ -121,11 +125,11 @@ class ScienceDirectAPI:
                     current_offset = future_to_offset[completed_future]
                     
                     if data is None:
-                        print(f"No more results available after offset {current_offset}.")
+                        logging.info(f"No more results available after offset {current_offset}.")
                         break
 
                     if 'resultsFound' not in data or 'results' not in data:
-                        print(f"Unexpected API response structure. Full response: {data}")
+                        logging.error(f"Unexpected API response structure. Full response: {data}")
                         return pd.DataFrame()
 
                     if total_results is None:
@@ -133,12 +137,12 @@ class ScienceDirectAPI:
 
                     results = data['results']
                     if not results:
-                        print(f"No more results available after offset {current_offset}.")
+                        logging.info(f"No more results available after offset {current_offset}.")
                         break
 
                     all_results.extend(results)
 
-                    print(f"Page {page} completed (offset {current_offset}). Total results so far: {len(all_results)}/{total_results}")
+                    logging.info(f"Page {page} completed (offset {current_offset}). Total results so far: {len(all_results)}/{total_results}")
                     page += 1
 
                     if len(all_results) >= total_results:
@@ -150,7 +154,7 @@ class ScienceDirectAPI:
                 offset += query['display']['show']
                 time.sleep(1)  # Add a small delay between requests
 
-        print(f"All available results retrieved. Total results: {len(all_results)}")
+        logging.info(f"All available results retrieved. Total results: {len(all_results)}")
 
         df = pd.DataFrame(all_results)
         
@@ -165,7 +169,7 @@ class ScienceDirectAPI:
     def scrape_all(self, max_workers=11):
         all_dfs = []
         for journal in self.journals:
-            print(f"Starting data retrieval for {journal}...")
+            logging.info(f"Starting data retrieval for {journal}...")
             query = {
                 "qs": "a OR b OR c OR d OR e OR f OR g OR h OR i OR j OR k OR l OR m OR n OR o OR p OR q OR r OR s OR t OR u OR v OR w OR x OR y OR z",
                 "pub": f'"{journal}"',
@@ -199,26 +203,36 @@ class ScienceDirectAPI:
             end = '&httpAccept=*%2F*'
             url = prefix + doi + postfix + self.api_key + end
 
-            response = requests.get(url)
+            try:
+                response = requests.get(url)
+                response.raise_for_status()
 
-            if response.status_code == 200:
                 file_name = f'/Users/stevensu/Desktop/SciSketch-Summer/Graphical Abstracts/{doi}.jpg'
                 with open(file_name, 'wb') as file:
                     file.write(response.content)
                 return True  # Graphical Abstract found
-            else:
-                return False  # Graphical Abstract not found
+
+            except requests.exceptions.RequestException as e:
+                logging.error(f"Failed to fetch graphical abstract for DOI {doi}: {e}")
+                return "failed"  # Mark as failed
 
         def process_row(row):
             doi = row['doi']
             row['GraphicalAbstract'] = API_call(doi)
             return row
 
-        with ThreadPoolExecutor(max_workers=max_workers) as executor:
-            futures = [executor.submit(process_row, row) for _, row in df.iterrows()]
-            for future in as_completed(futures):
-                row = future.result()
-                df.loc[df['doi'] == row['doi'], 'GraphicalAbstract'] = row['GraphicalAbstract']
+        # Progress bar setup
+        with tqdm(total=len(df), desc="Processing DOIs", unit="doi") as pbar:
+            with ThreadPoolExecutor(max_workers=max_workers) as executor:
+                futures = [executor.submit(process_row, row) for _, row in df.iterrows()]
+                for future in as_completed(futures):
+                    try:
+                        row = future.result()
+                        df.loc[df['doi'] == row['doi'], 'GraphicalAbstract'] = row['GraphicalAbstract']
+                    except Exception as e:
+                        logging.error(f"Error processing row: {e}")
+                    finally:
+                        pbar.update(1)
 
         return df
 
@@ -228,7 +242,8 @@ if __name__ == "__main__":
     df = sd_api.scrape_all()
     
     # Count the number of rows where 'GraphicalAbstract' is True
-    count_true = df['GraphicalAbstract'].sum()
+    count_true = df['GraphicalAbstract'].eq(True).sum()
 
+    df.to_csv('output.csv', index=False)
     # Print the count
-    print(f"Number of rows with 'GraphicalAbstract' = True: {count_true}")
+    logging.info(f"Number of rows with 'GraphicalAbstract' = True: {count_true}")
